d925aec22023d345c24178f402e82b148bc06300
Sumit <sumit.raghuwanshi192@gmail.com>
2025-06-03 09:16:39 +0530

    Handle one and two numbers input

--- spec/services/string_calculator_service_spec.rb
+++ spec/services/string_calculator_service_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculatorService do
     it 'returns 0 for an empty string' do
       expect(StringCalculatorService.add("")).to eq(0)
     end
+
+    it 'returns the number itself when only one number is provided' do
+      expect(StringCalculatorService.add("1")).to eq(1)
+    end
+
+    it 'returns the sum when two numbers are provided separated by comma' do
+      expect(StringCalculatorService.add("1,5")).to eq(6)
+    end
   end
 end
